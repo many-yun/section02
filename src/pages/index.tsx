c4f82b28bbfaf0f1507cc1bee4c1a2452c6ff42c
dykim <onurleft@naver.com>
2024-12-18 11:21:01 +0900

    2.11 SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,46 @@
+import { ReactNode } from "react";
 import style from "./index.module.css"
+import SearchableLayout from "@/components/searchable-layout";
+import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks()
+  ])
+
+  return {
+    props: {
+      allBooks,
+      recoBooks
+    }
+  }
+}
+
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
   return (
-    <>
-      <h1 className={style.h1}>인덱스</h1>
-      <h2 className={style.h2}>H2</h2>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {recoBooks.map(book => <BookItem key={book.id} {...book}></BookItem>)}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {allBooks.map(book => <BookItem key={book.id} {...book}></BookItem>)}
+      </section>
+    </div>
   );
+}
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>
 }
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,8 +1,31 @@
-import { useRouter } from "next/router"
+import { ReactNode } from "react"
+import SearchableLayout from "@/components/searchable-layout"
+import BookItem from "@/components/book-item"
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next"
+import fetchBooks from "@/lib/fetch-books"
 
-export default function Search () {
-  const router = useRouter()
-  const { q } = router.query
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q
+  const books = await fetchBooks(q as string)
 
-  return <h1>search {q}</h1>
+  return {
+    props: {
+      books
+    }
+  }
+}
+
+export default function Search ({
+  books
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+  return <div>
+    {books.map(book => <BookItem key={book.id} {...book}></BookItem>)}
+  </div>
+}
+
+Search.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>
 }
